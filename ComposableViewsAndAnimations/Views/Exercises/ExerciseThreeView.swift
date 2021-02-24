b6ad2f6f4161d5da3d69516e94b56eff7c892540
Russell Gordon <rgordon@lcs.on.ca>
2021-02-24 18:13:15 -0500

    Explored the use of the .trim modifier that traces the outline of a shape. Can use this for a progress meter, for example.

--- ComposableViewsAndAnimations/Views/Exercises/ExerciseThreeView.swift
+++ ComposableViewsAndAnimations/Views/Exercises/ExerciseThreeView.swift
@@ -14,49 +14,40 @@ struct ExerciseThreeView: View {
     
     // Controls whether this view is showing or not
     @Binding var showThisView: Bool
-        
-    // Controls what typeface the text is shown in
-    @State private var typeFace: String = "Helvetica-Neue"
-
-    // Whether to apply the animation
-    @State private var useAnimation = false
-
-    // MARK: Computed properties
-
-    // List all fonts available
-    // NOTE: This is a very useful gist...
-    //       https://gist.github.com/kristopherjohnson/c825cb97b1ad1fe0bc13d709986d0763
-    private static let fontNames: [String] = {
-        var names: [String] = []
-        for familyName in UIFont.familyNames {
-            names.append(contentsOf: UIFont.fontNames(forFamilyName: familyName))
-        }
-        return names.sorted()
-    }()
-
+    
+    @State private var completionAmount: CGFloat = 0.0
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         
         NavigationView {
-            
-            VStack {
-                
-                // NOTE: Here are some neat examples to consider...
-                //       https://medium.com/better-programming/create-an-awesome-loading-state-using-swiftui-9815ff6abb80
-                Text(typeFace)
-                    .font(.custom(typeFace, size: 30.0))
-                
-            }
-            .navigationTitle("Exercise 3")
-            .toolbar {
-                ToolbarItem(placement: .primaryAction) {
-                    Button("Done") {
-                        hideView()
+            Circle()
+                // Traces, or makes a trim, for the outline of a shape
+                .trim(from: 0, to: completionAmount)
+                .stroke(Color.red, lineWidth: 20)
+                .frame(width: 200, height: 200)
+                .rotationEffect(.degrees(-90))
+                .onReceive(timer) { _ in
+                    
+                    // Animate the trim being closed completely on the circle
+                    withAnimation(.easeOut(duration: 3.0)) {
+                        completionAmount = 1
+                    }
+                    
+                    // Stop the timer from continuing to fire
+                    timer.upstream.connect().cancel()
+                }
+                .navigationTitle("Exercise 3")
+                .toolbar {
+                    ToolbarItem(placement: .primaryAction) {
+                        Button("Done") {
+                            hideView()
+                        }
                     }
                 }
-            }
-
         }
-        
+
     }
     
     // MARK: Functions
